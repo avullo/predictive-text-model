d58b08f9af0982c583a373f5cb3a06205e9bff5b
Alessandro Vullo <avullo@ebi.ac.uk>
2017-12-04 16:23:53 +0000

    Function to create ngram tables to support model building.

--- lib/ngram.R
+++ lib/ngram.R
@@ -4,6 +4,37 @@ library(tidytext)
 # library(R.utils)
 # library(data.table)
 
+ngram_table <- function(data, size = 1) {
+  stopifnot(!is.null(data$text))
+  stopifnot(size >= 1 & size <= 4)
+  
+  table <- 
+    tokenise(data, size) %>%
+    count(w, sort = TRUE) %>%
+    mutate(w = reorder(w,n))
+  
+  # return in the simplest (unigram) case,
+  # rename tok col for consistency with other cases
+  if(size == 1) { return(table %>% rename(w1 = w)) }
+  
+  # split ngrams (n > 1) token column into one column for each word
+  cols <- c()
+  for (i in seq(size)) {
+    cols <- c(cols, paste("w", i, sep = ""))
+  }
+  table %>%
+    separate(w, cols, sep = " ")
+} 
+
+tokenise <- function(data, size) {
+  if(size == 1) {
+    return(data %>% unnest_tokens(w, text))
+  }
+  
+  data %>%
+    unnest_tokens(w, text, token = "ngrams", n = size)
+}
+
 #' create an Ngram tokenizer
 #' @param ng, an integer, indicates length of the ngrams, i.e., for ng = 2, the
 #' text will be splitted in bigrams, for ng = 3, in trigrams, etc.
@@ -14,11 +45,4 @@ tokenizer <- function(ng) {
   #' @return a vector containing the ngrams
   t <- function(x)
     unlist(tokenize_ngrams(x, n = ng))
-}
-
-tokenize_into_ngram <- function(data, size = 1) {
-  stopifnot(!is.null(data$text))
-  
-  data %>%
-    unnest_tokens(w, text, token = "ngrams", n = size)
 }
