1470b683ca3b7004d1ba9d3bd6d88fdf3b1815b1
Alessandro Vullo <avullo@ebi.ac.uk>
2017-12-06 17:18:37 +0000

    Template script for processing (i.e. cleaning) sampled data.

---
+++ src/03_process_data.R
@@ -0,0 +1,48 @@
+#!/usr/bin/env Rscript
+
+library(logging)
+library(kimisc) # for thisfile() to determine the path of the currently running script
+
+# config logging
+basicConfig()
+
+# set working directory to roor dir
+this.file <- normalizePath(thisfile())
+setwd(file.path(dirname(this.file), '..'))
+loginfo(sprintf("Set working directory to %s", getwd()))
+
+source("lib/helpers.R")
+
+loginfo("Reading configuration")
+library(yaml)
+config <- yaml.load_file("config.yml")
+
+locale <- config$locale
+
+sample_perc <- config$data$sample
+assert(sample_perc > 0 & sample_perc <= 100, "Data sample perc must be a number > 0 and <= 100")
+loginfo(sprintf("Will use %s %d perc sample data", locale, sample_perc))
+
+# check existence of sampled data 
+raw_data_dir <- file.path(config$data$dir, config$data$raw$dir)
+sample_data_fname <- file.path(raw_data_dir, paste(locale, '.sample.', sample_perc, '.rds', sep = ""))
+assert(file.exists(sample_data_fname), 
+       sprintf("sample data file %s does not exist", sample_data_fname))
+
+# TODO
+# process sample data
+# corpus <- readRDS(sample_data_fname)
+
+# create processed data dir if not exist
+processed_data_dir <- file.path(config$data$dir, config$data$process$dir)
+if(!file.exists(processed_data_dir)) {
+  loginfo(sprintf("Processed data dir %s does not exist, creating", processed_data_dir))
+  dir.create(processed_data_dir, showWarnings = FALSE, recursive = TRUE)
+}
+
+# save processed sampled data to file
+processed_data_fname <- file.path(processed_data_dir, 
+                                  paste(locale, '.sample.', sample_perc, '.rds', sep = ""))
+saveRDS(object = processed_corpus, file = procesed_data_fname)
+
+loginfo("Done")
