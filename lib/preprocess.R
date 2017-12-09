3a7927fc63b1150283f800d41656e3516bb8b894
Alessandro Vullo <avullo@ebi.ac.uk>
2017-12-09 15:27:37 +0000

    Started processing text with various bits assembled, neither complete nor completely correct.

--- lib/preprocess.R
+++ lib/preprocess.R
@@ -7,23 +7,27 @@ library(dplyr)
 # characters during tokenisation except underscores
 markers <- list(number = '_num_', email = '_email_', url = '_url_', hashtag = '_hashtag_', twname = '_twname_', profane = '_profane_', eos = '_eos_')
 
-preprocess <- function(txt) {
-  txt
-    %>% process_numbers() 
-    %>% process_emails()  
-    %>% process_urls()  
-    %>% process_twitter() 
-    %>% process_slang()       # TODO
-    %>% process_profanity()   # TODO
-    %>% process_punctuation() 
+# TODO
+# stemming and stopwords
+preprocess <- function(txt, stem = F, stopwords = F) {
+   txt %>% 
+     process_numbers()     %>% 
+     process_emails()      %>% 
+     process_urls()        %>% 
+     process_twitter()     %>% 
+     process_slang()       %>%      # TODO
+     process_profanity()   %>%  # TODO
+     process_punctuation()    # TODO
 }
 
 process_numbers <- function(txt) {
-  gsub("(^|\s)[0-9]+([,\\.]?[0-9]+)?", "_num_", txt)
+  gsub("(^\\s*?|[.!?]+\\s*?|\\s)[0-9]+([,\\.]?[0-9]+)?\\s", " _num_ ", txt)
 }
 
 process_emails <- function(txt) {
-  gsub("\\w*@\\w*\\.\\w*", "_email_", txt)
+  # the simplest regex possible, match also invalid email addresses
+  gsub("\\w+[-+.]*\\w+@.+\\.\\w+", "_email_", txt)
+  # gsub("\\w*@\\w*\\.\\w*", " _email_ ", txt, perl = TRUE)
 }
 
 process_urls <- function(txt) {
@@ -37,15 +41,18 @@ process_twitter <- function(txt) {
   # {} and use . as a data "marker".
   # https://stackoverflow.com/questions/39997273/r-combine-several-gsub-function-ina-pipe
   
-  txt
-    %>% { gsub("(\\#[[:alnum:]]+)", "_hashtag_", .) }     # hash tags
-    %>% { gsub("\\@\\w*[[:alnum]]+\\w*", "_twname_", .) } # twitter names
-    %>% { gsub("\\b(rt|RT)\\b", " ", .) }                 # remove retweets
+  gsub("(\\#[[:alnum:]]+)", "_hashtag_", txt)         %>% # hash tags
+    { gsub("\\@\\w*[[:alnum]]+\\w*", "_twname_", .) } %>% # twitter names
+    { gsub("\\b(rt|RT)\\b", " ", .) }                     # remove retweets
 }
 
 # TODO
 process_slang <- function(txt) {
-  txt
+  gsub("\\s[b]\\s", " be ", txt)            %>%
+     { gsub("\\s[n]\\s", " and ", .) }      %>%
+     { gsub("\\s[u]\\s", " you ", .) }      %>%
+     { gsub("\\s[t]\\s", " too ", .) }      %>%
+     { gsub("\\sb4\\s",  " before ", .) }
 }
 
 # TODO
@@ -53,6 +60,7 @@ process_profanity <- function() {
   txt  
 }
 
+# TODO
 process_punctuation <- function(txt) {
   gsub("[.!?]+", " _eos_ ", txt) # replace end of sentence punctuation with special marker
 }
